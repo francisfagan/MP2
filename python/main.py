d50829f4fa1220880588de8027f5edf667199774
frank <francis.fagan@ucdconnect.ie>
2026-05-18 12:05:17 +0100

    Added matrix initialisation in Simulation class. Also fixed some syntax in bodies.py

--- python/main.py
+++ python/main.py
@@ -2,25 +2,32 @@ import numpy as np
 import matplotlib.pyplot as plt
 from bodies import Body, Sun, Earth, Jupiter, Mars
 
+np.set_printoptions(linewidth=100)
+
 # constants
 G = 6.674e-20
-
+Au =  149597870.700 # in kilometers
 
 class Simulation:
     def __init__(self, bodies) -> None:
         self.bodies = bodies
         self.N_bodies = len(bodies)
         self.Ndim = 6
-        self.quant_vec = np.concatenate([n.return_vec() for n in self.bodies])
+        self.mat = self.initiliase_matrix(bodies)
+        self.dt = 86400 # 1 day in seconds
+
+    def initiliase_matrix(self, bodies):
+        return np.array([body.return_vec()/(Au) for body in bodies], dtype=float)
 
     def print_vector(self):
-        print(self.quant_vec)
+        print(self.mat)
 
     def rk4(self):
+        pass
 
 
 
 
-bodies = [Earth, Sun, Mars, Jupiter]
+bodies = [Sun, Mars, Earth, Jupiter]
 mySim = Simulation(bodies)
 mySim.print_vector()
