027bab3e7362965ae9fce012948060cc8f093c91
frank <francis.fagan@ucdconnect.ie>
2026-05-19 12:59:58 +0100

    Got the simulation working with RK4. Also added a progress bar and plotting at the end. It's not real-time plotting yet however and the plot is simply all the positions for the planets. Distances are still calculated in km which may need to change.

--- python/main.py
+++ python/main.py
@@ -1,29 +1,49 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from bodies import Body, Sun, Earth, Jupiter, Mars, Mercury, Neptune, Uranus, Venus, Ganymede
+from tqdm import tqdm
+
+from bodies import (
+    Body,
+    Sun,
+    Earth,
+    Jupiter,
+    Mars,
+    Mercury,
+    Neptune,
+    Uranus,
+    Venus,
+    Ganymede,
+    Titan,
+    Callisto,
+    Io,
+    Moon,
+    Europa,
+    Triton,
+)
 
 # constants
 G = 6.674e-20
 Au = 149597870.700  # in kilometers
 
-np.set_printoptions(linewidth=100)
+np.set_printoptions(linewidth=100) # simply allows printing matrices to the screen to be more readable
 
 
 class Simulation:
-    def __init__(self, bodies) -> None:
+    def __init__(self, bodies: list[Body]) -> None:
         self.bodies = bodies
         self.N_bodies = len(bodies)
         self.Ndim = 6
         self.masses = np.array([body.mass for body in bodies], dtype=float)
         self.mat = self.initiliase_matrix(bodies)
+        self.history : np.ndarray # leave history as just array for now, uninitialised
 
-    def initiliase_matrix(self, bodies) -> np.ndarray:
+    def initiliase_matrix(self, bodies: list[Body]) -> np.ndarray:
         return np.array([body.return_vec() for body in bodies], dtype=float)
 
     def print_matrix(self) -> None:
         print(self.mat)
 
-    def dmatrix(self, mat) -> np.ndarray:
+    def dmatrix(self, mat: np.ndarray) -> np.ndarray:
         """
         This function takes in the matrix that consists of the velocity and the position, where the first three coloumns ae
         It returns a matrix of acceleration and velocity, where velocity is the first three coloumns and acceleration is the last three coloumns.
@@ -45,7 +65,7 @@ class Simulation:
 
         return dmat
 
-    def rk4(self, mat, dt) -> np.ndarray:
+    def rk4(self, mat: np.ndarray, dt: int) -> np.ndarray:
         k1 = self.dmatrix(mat)
         k2 = self.dmatrix(mat + 0.5 * dt * k1)
         k3 = self.dmatrix(mat + 0.5 * dt * k2)
@@ -53,19 +73,47 @@ class Simulation:
 
         return mat + (dt / 6.0) * (k1 + 2 * k2 + 2 * k3 + k4)
 
-    def run(self, dt, T, t0) -> None:
+    def run(self, dt: int, T: int, t0: int) -> None:
         t = t0
-        n = 0
-        while t < T:
-            self.mat = self.rk4(self.mat, dt)
-            print(self.mat)
-            n += 1
-            t += dt
-        print(n)
+        n_steps = int((T - t0)/dt)
+        history = [self.mat[:, :3].copy()] # only need positions, forgo the velocity columns
+        for _ in tqdm(range(n_steps)): # just a progress bar
+            self.mat = self.rk4(self.mat, dt) # rk4 iterations
+            history.append(self.mat[:, :3].copy())
+        self.history = np.array(history) # return history at the end for plotting
 
+bodies = [
+    Sun,
+    Earth,
+    Jupiter,
+    Mars,
+    Mercury,
+    # Neptune,
+    # Uranus,
+    Venus,
+    # Ganymede,
+    # Titan,
+    # Callisto,
+    # Io,
+    Moon,
+    # Europa,
+    # Triton,
+]
 
-bodies = [Sun, Earth, Jupiter, Mars, Mercury, Neptune, Uranus, Venus, Ganymede]
-T = 3 * 86400  # one year in seconds
-dt = 86400
+dt = 3600 # 1 hour time steps
+T = 2 * 365 * 24 * 3600 # simulation lasts a year
+
 mySim = Simulation(bodies)
-mySim.run(dt, T, 0)
+mySim.run(dt, T, t0=0)
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+for i, body in enumerate(mySim.bodies):
+    x = mySim.history[:, i, 0]
+    y = mySim.history[:, i, 1]
+    z = mySim.history[:, i, 2]
+    ax.plot(x, y, z, label=body.name)
+
+# ax.legend() # can uncomment for legend, blocks the view tho
+plt.show()
